c0924bd1181de11009e3c5b934100ebc4804a8eb
Sokkong <sokkonghoun1@gmail.com>
2024-03-26 15:58:05 +0700

    removing img

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <main className="mx-auto max-w-5xl px-4 sm:px-6 lg:max-w-screen-2xl"></main>
+  );
 };
 
 export default Products;
