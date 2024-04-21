afd48cd5d01bebeae71c71e381febdb23755276c
Sokkong <sokkonghoun1@gmail.com>
2024-04-21 19:22:43 +0700

    adding table contents to product page and dashboard page

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { LineChart } from "@mui/x-charts/LineChart";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="bg-white mx-auto max-w-4xl px-4 sm:px-6 lg:max-w-screen-2xl">
+      <LineChart
+        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+        series={[
+          {
+            data: [2, 5.5, 2, 8.5, 1.5, 5],
+          },
+        ]}
+        height={300}
+        margin={{ left: 30, right: 30, top: 30, bottom: 30 }}
+        grid={{ vertical: true, horizontal: true }}
+      />
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/pages/admin/ProductDashboard.jsx
+++ src/pages/admin/ProductDashboard.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import EnhancedTable from "../../components/admin/ProductTable";
 
 const ProductDashboard = () => {
-  return <div>ProductDashboard</div>;
+  return (
+    <div className="mx-auto max-w-4xl px-4 sm:px-6 lg:max-w-screen-2xl">
+      <EnhancedTable />
+    </div>
+  );
 };
 
 export default ProductDashboard;
