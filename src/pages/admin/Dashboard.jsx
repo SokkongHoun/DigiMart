e6a6db91e8f8ca84f47d172c8673b98e2e6543e2
Sokkong <sokkonghoun1@gmail.com>
2024-05-13 11:13:40 +0700

    adding chart to product dashboard page

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { LineChart } from "@mui/x-charts/LineChart";
 import { Link } from "react-router-dom";
+import { BarChart } from "@mui/x-charts/BarChart";
+import { PieChart } from "@mui/x-charts/PieChart";
 
 const Dashboard = () => {
+  /* Today's sale with top most sell product */
   const LineBoard = () => {
     return (
       <>
@@ -13,7 +16,6 @@ const Dashboard = () => {
               data: [2, 5.5, 2, 8.5, 1.5, 5],
             },
           ]}
-          height={300}
           margin={{ left: 30, right: 30, top: 30, bottom: 30 }}
           grid={{ vertical: true, horizontal: true }}
           sx={{
@@ -43,13 +45,66 @@ const Dashboard = () => {
     );
   };
 
+  /* Total sales */
+  const StatChart = () => {
+    return (
+      <div className="stats shadow w-full">
+        <div className="stat w-full">
+          <div className="stat-title">Total Page Views</div>
+          <div className="stat-value">89,400</div>
+          <div className="stat-desc">21% more than last month</div>
+        </div>
+        <div className="stat w-full">
+          <div className="stat-title">Total Page Views</div>
+          <div className="stat-value">89,400</div>
+          <div className="stat-desc">21% more than last month</div>
+        </div>
+      </div>
+    );
+  };
+
+  /* Top 3 categories, each category represent the top product from that category*/
+  const PieChartData = () => {
+    const data = [
+      { id: 0, value: 10, label: "series A" },
+      { id: 1, value: 15, label: "series B" },
+      { id: 2, value: 20, label: "series C" },
+    ];
+
+    return (
+      <PieChart
+        series={[
+          {
+            data,
+            highlightScope: { faded: "global", highlighted: "item" },
+            faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
+          },
+        ]}
+        height={200}
+      />
+    );
+  };
+
+  /* Top 3 buyers table */
+  /* Top 3 most sales product of all time table */
+
   return (
     <div>
       <div className="drawer lg:drawer-open ">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content">
+        <div className="drawer-content grid grid-cols-[800px,1fr] gap-1 h-2/3">
           {/* Page content here */}
-          <LineBoard />
+          <div className="col-span-1 rounded-md border border-neutral-500 m-1">
+            <LineBoard />
+          </div>
+          <div className="col-span-1 border-neutral-500 flex flex-col justify-between h-full m-1">
+            <div className="border rounded-md">
+              <StatChart />
+            </div>
+            <div className="border rounded-md flex-grow flex-shrink-0 flex items-center justify-center mt-3">
+              <PieChartData />
+            </div>
+          </div>
         </div>
         <div className="drawer-side">
           <label
