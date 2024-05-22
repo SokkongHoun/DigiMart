6b4bef718dde3fa43a5d3a4bac6df64c91829c33
Sokkong <sokkonghoun1@gmail.com>
2024-05-22 18:42:58 +0700

    replace img in nav

--- src/components/admin/AdminNav.jsx
+++ src/components/admin/AdminNav.jsx
@@ -65,7 +65,7 @@ const AdminNav = () => {
               <div className="w-10 rounded-full">
                 <img
                   alt="Tailwind CSS Navbar component"
-                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                  src="../../public/administrator.png"
                 />
               </div>
             </div>
